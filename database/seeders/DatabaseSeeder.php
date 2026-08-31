ae49170e7d0708b91977581c93d60400ea8d8133
matheusbrito <matheusbrito@MacBook-Air-de-matheusbrito.local>
2026-08-31 13:02:17 -0300

    fix(seeder): use User::firstOrCreate directly in DatabaseSeeder and include fakerphp/faker in require

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+            ]
+        );
     }
 }
